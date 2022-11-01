f16c8548b7e98a2bfb9950b4ecf9221a7a2387ad
saracsilva <saracarolinasilva@gmail.com>
2022-11-01 21:05:52 +0100

    iteration_2 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -24,4 +24,22 @@ router.post("/signup", async (req, res) => {
 router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const currentUser = await User.findOne({ username });
+  if (!currentUser) {
+    res.render("auth/login", { errorMessage: "No user with that username" });
+  } else {
+    if (bcrypt.compareSync(password, currentUser.password)) {
+      console.log("correct password");
+      /* const sessionUser = structuredClone(currentUser);
+      delete sessionUser.password; */
+      req.session.user = currentUser;
+      res.redirect("/profile");
+    } else {
+      res.render("auth/login", { errorMessage: "incorrect password" });
+    }
+  }
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,12 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
-
+router.get("/profile", (req, res) => {
+  console.log("SESSION =====> ", req.session);
+  if (req.session.user) {
+    res.render("auth/profile", { user: req.session.user, isConnected: true });
+  } else {
+    res.redirect("/auth/login");
+  }
+});
 module.exports = router;
